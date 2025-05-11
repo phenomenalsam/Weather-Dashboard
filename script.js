4b27c1bc6e3c622f7804eb04f103296bc83e54e4
phenomenalsam <subhamyadav810255@gmail.com>
2025-05-11 15:15:37 +0530

    add necessary files and code

---
+++ script.js
@@ -0,0 +1,19 @@
+async function getWeather() {
+  const city = document.getElementById("cityInput").value;
+  const apiKey = "YOUR_API_KEY";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.cod === 200) {
+    document.getElementById("weatherResult").innerHTML =`
+      <p><strong>${data.name}</strong></p>
+      <p>Temperature: ${data.main.temp}°C</p>
+      <p>Humidity: ${data.main.humidity}%</p>
+      <p>Weather: ${data.weather[0].description}</p>
+    `;
+  } else {
+    document.getElementById("weatherResult").innerHTML = <p>City not found.</p>;
+  }
+}
